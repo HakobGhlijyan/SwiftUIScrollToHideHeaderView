8c93b8bd98932890ee7891571061d51e1a96d4b4
Ghlijyan Hakob <hakobghlijyan@gmail.com>
2024-11-10 13:49:42 +0400

    Initial Commit

---
+++ SwiftUIScrollToHideHeaderView/SwiftUIScrollToHideHeaderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollToHideHeaderViewApp.swift
+//  SwiftUIScrollToHideHeaderView
+//
+//  Created by Hakob Ghlijyan on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollToHideHeaderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
